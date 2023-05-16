972928148667dafb1228b83da11cdf1e133393c2
aldalddl <aldalddl2007@gmail.com>
2023-05-16 23:29:27 +0900

    이진변환반복하기

---
+++ Algorithm/Algorithm/이진변환반복하기.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solutionS(_ s:String) -> [Int] {
+    var x = s
+    var zeroC = 0
+    var c = 0
+    var convertC = 0
+    
+    while x != "1" {
+        zeroC += x.filter({ $0 == "0" }).count
+        c = x.filter({ $0 == "1" }).count
+        x = String(c, radix: 2)
+        convertC += 1
+    }
+    
+    return [convertC, zeroC]
+}
+
+print(solutionS("110010101001"))
