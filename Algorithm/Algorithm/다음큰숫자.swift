d48ed41b79c749b89ce37f82890135404ec52634
aldalddl <aldalddl2007@gmail.com>
2023-05-23 23:56:42 +0900

    다음큰숫자

---
+++ Algorithm/Algorithm/다음큰숫자.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var binary = String(n, radix: 2)
+    var count1 = binary.filter({$0 == "1"}).count
+    var nextN = n + 1
+
+    while true {
+        binary = String(nextN, radix: 2)
+        
+        if binary.filter({$0 == "1"}).count == count1 {
+            return nextN
+        }
+        
+        nextN += 1
+    }
+}
