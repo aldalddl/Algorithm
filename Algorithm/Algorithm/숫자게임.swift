e1c129c326478c7df0d0b4a48c62c64285402232
aldalddl <aldalddl2007@gmail.com>
2023-05-29 00:05:18 +0900

    숫자게임

--- Algorithm/Algorithm/숫자게임.swift
+++ Algorithm/Algorithm/숫자게임.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 func solution(_ a:[Int], _ b:[Int]) -> Int {
-    var a = a
-    var b = b
+    var sortedA = a.sorted(by: >)
+    var sortedB = b.sorted(by: >)
     var count = 0
     var indexB = 0
-    
-    a.sort()
-    b.sort()
 
-    for i in 0..<a.count {
-        if a[i] < b[indexB] {
-            indexB += 1
+    for i in 0..<sortedA.count {
+        if sortedA[i] < sortedB[indexB] {
             count += 1
+            indexB += 1
         }
     }
+    
     return count
 }
+
+print(solution([5,1,3,7])
