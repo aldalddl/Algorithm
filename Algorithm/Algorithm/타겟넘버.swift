9a5329c9c17bb7025fdccf1611b4d14f99ef858d
aldalddl <aldalddl2007@gmail.com>
2023-06-07 22:57:17 +0900

    타겟넘버

--- Algorithm/Algorithm/타겟넘버.swift
+++ Algorithm/Algorithm/타겟넘버.swift
@@ -1,33 +1,22 @@
-//
-//  타겟넘버.swift
-//  Algorithm
-//
-//  Created by MinJi Kang on 2023/03/23.
-//
-
 import Foundation
 
-let numbers = [1,1,1,1,1]
-let target = 3
-
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    var targetCount = 0
-
+    var count = 0
+    
     func dfs(index: Int, sum: Int) {
-        if index == (numbers.count - 1) && sum == target {
-            targetCount += 1
+        if index == numbers.count - 1 {
+            if sum == target {
+                count += 1
+            }
             return
         }
-
-        guard let index < numbers.count - 1 else { return }
-
+        
         dfs(index: index + 1, sum: sum + numbers[index + 1])
         dfs(index: index + 1, sum: sum - numbers[index + 1])
     }
-
+    
     dfs(index: -1, sum: 0)
-
-    return targetCount
+    
+    return count
 }
-
-print(solution(numbers, target))
+print(solution([1, 1, 1, 1, 1], 3))
